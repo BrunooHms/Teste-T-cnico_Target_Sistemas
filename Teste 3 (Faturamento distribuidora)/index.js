ba0897c94d52e014fbf17ef15d1e130598f549d9
BrunooHms <Bruno_Hms@outlook.com>
2024-09-03 19:10:57 -0300

    Atualização do teste a pedido da empresa

--- Teste 3 (Faturamento distribuidora)/index.js
+++ Teste 3 (Faturamento distribuidora)/index.js
@@ -1,15 +1,8 @@
-// Infelizmente não encontrei nenhum arquivo JSON no teste técnico, então simulei uma entrada para desenvolver o código.
+const fs = require('fs');
+const path = require('path');
 
-const faturamentoMensal = [
-    {"dia": 1, "valor": 22174.1664},
-    {"dia": 2, "valor": 24537.6698},
-    {"dia": 3, "valor": 26139.6134},
-    {"dia": 4, "valor": 0.0},
-    {"dia": 5, "valor": 0.0},
-    {"dia": 6, "valor": 26742.6612},
-    {"dia": 7, "valor": 0.0},
-    {"dia": 8, "valor": 42889.2258},
-];
+const dadosArquivo = fs.readFileSync(path.resolve(__dirname, 'dados.json'));
+const faturamentoMensal = JSON.parse(dadosArquivo);
 
 
 const diasComFaturamento = faturamentoMensal.filter(dia => dia.valor > 0);
